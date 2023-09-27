b017141bdd1fba321ae179b68255266f589c2707
ziloet <rneutron@protonmail.com>
2023-09-27 14:43:52 +0300

    add debug build flags

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ if /i "%1" equ "clean" (
 	exit /b 0
 )
 
-set msvc_flags=/nologo /Zi /W3 /D TOYREND_SYSTEM_WINDOWS /D TOYREND_COMPILER_MSVC
+set msvc_flags=/nologo /W3 /D TOYREND_SYSTEM_WINDOWS /D TOYREND_COMPILER_MSVC /Zi /D TOYREND_BUILD_DEBUG
 set link_flags=/WX /subsystem:windows /incremental:no /opt:ref,icf
 set lib_files=kernel32.lib user32.lib gdi32.lib
 call cl toyrend.c /Fe:toyrend.exe %msvc_flags% /link %link_flags% %lib_files%
